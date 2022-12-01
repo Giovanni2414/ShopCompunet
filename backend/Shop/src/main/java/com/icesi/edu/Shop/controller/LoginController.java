dc4d02c1c309148448ec3b7e3cedda010e5bc895
Giovanni Mosquera <Giovanni2414g@gmail.com>
2022-12-01 14:26:03 -0500

    Added cros

--- backend/Shop/src/main/java/com/icesi/edu/Shop/controller/LoginController.java
+++ backend/Shop/src/main/java/com/icesi/edu/Shop/controller/LoginController.java
@@ -5,10 +5,12 @@ import com.icesi.edu.Shop.dto.LoginDTO;
 import com.icesi.edu.Shop.dto.TokenDTO;
 import com.icesi.edu.Shop.service.LoginService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
+@CrossOrigin(origins = "*")
 public class LoginController implements LoginAPI {
 
 
---
+++ backend/Shop/src/main/java/com/icesi/edu/Shop/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.icesi.edu.Shop.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@CrossOrigin(origins = "*")
+public class UserController {
+}
